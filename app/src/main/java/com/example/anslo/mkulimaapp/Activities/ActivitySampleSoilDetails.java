f24760e5453eac7bca5657023ae88e66d18ec7e4
Paul Mburu <paulmburu53@gmail.com>
2018-11-01 20:22:58 +0300

    Added OnClickListener to add new soil sample

---
+++ app/src/main/java/com/example/anslo/mkulimaapp/Activities/ActivitySampleSoilDetails.java
@@ -0,0 +1,16 @@
+package com.example.anslo.mkulimaapp.Activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by anslo on 11/1/18.
+ */
+
+public class ActivitySampleSoilDetails extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
