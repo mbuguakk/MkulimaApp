7eb16746791e22704302edaab8c36820bc6d2e3a
Paul Mburu <paulmburu53@gmail.com>
2018-11-03 23:43:07 +0300

    List items with SampleSoil object data type

--- app/src/main/java/com/example/anslo/mkulimaapp/Data/Constant.java
+++ app/src/main/java/com/example/anslo/mkulimaapp/Data/Constant.java
@@ -13,22 +13,16 @@ public class Constant {
 
     public final String TAG= Constant.class.getSimpleName();
 
-    public static List<String> getSoilSamples(Context ctx){
-        List<String> mSoilSample=new ArrayList<String>();
-        mSoilSample.add("Sample Soil 1");
-        mSoilSample.add("Sample Soil 2");
-        mSoilSample.add("Sample Soil 3");
-        mSoilSample.add("Sample Soil 4");
-        mSoilSample.add("Sample Soil 5");
-        mSoilSample.add("Sample Soil 6");
-        mSoilSample.add("Sample Soil 7");
-        mSoilSample.add("Sample Soil 8");
-        mSoilSample.add("Sample Soil 9");
-        mSoilSample.add("Sample Soil 10");
-        mSoilSample.add("Sample Soil 11");
-        mSoilSample.add("Sample Soil 12");
-        mSoilSample.add("Sample Soil 13");
-        mSoilSample.add("Sample Soil 14");
+    public static List<SampleSoil> mSoilSample=new ArrayList<SampleSoil>();;
+
+    public static List<SampleSoil> getSoilSamples(Context ctx){
+        mSoilSample.add(new SampleSoil("Sample Soil 1"));
+        mSoilSample.add(new SampleSoil("Sample Soil 2"));
+        mSoilSample.add(new SampleSoil("Sample Soil 3"));
+        mSoilSample.add(new SampleSoil("Sample Soil 4"));
         return mSoilSample;
     }
+
+
+
 }
