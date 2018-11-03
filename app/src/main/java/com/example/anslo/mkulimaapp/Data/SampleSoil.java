ca0d40e531ece03f6638c855fc2dac71e8a7da4c
Paul Mburu <paulmburu53@gmail.com>
2018-11-03 23:52:21 +0300

    SampleSoil class

---
+++ app/src/main/java/com/example/anslo/mkulimaapp/Data/SampleSoil.java
@@ -0,0 +1,19 @@
+package com.example.anslo.mkulimaapp.Data;
+
+/**
+ * Created by anslo on 11/3/18.
+ */
+
+public class SampleSoil {
+    private String soilName;
+
+    public SampleSoil(String name){this.soilName=name;}
+
+    public void setSoilName(String soilName) {
+        this.soilName = soilName;
+    }
+
+    public String getSoilName() {
+        return soilName;
+    }
+}
