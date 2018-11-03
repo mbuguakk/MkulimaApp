d24b837915adcca4b3e10927b711e22f88ac5c21
Paul Mburu <paulmburu53@gmail.com>
2018-11-03 23:41:35 +0300

    Setup passing of data from one activity to the other

--- app/src/main/java/com/example/anslo/mkulimaapp/Activities/ActivitySampleSoilDetails.java
+++ app/src/main/java/com/example/anslo/mkulimaapp/Activities/ActivitySampleSoilDetails.java
@@ -1,16 +1,34 @@
 package com.example.anslo.mkulimaapp.Activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.example.anslo.mkulimaapp.R;
 
 /**
- * Created by anslo on 11/1/18.
+ * Created by Paul Mburu on 11/1/18.
  */
 
 public class ActivitySampleSoilDetails extends AppCompatActivity {
+
+    public static final String EXTRA_OBJCT = "key.OBJCT";
+
+    // give preparation animation activity transition
+    public static void navigate(AppCompatActivity activity, View transitionImage, String obj) {
+        Intent intent = new Intent(activity, ActivitySampleSoilDetails.class);
+        intent.putExtra(EXTRA_OBJCT, obj);
+        ActivityOptionsCompat options = ActivityOptionsCompat.makeSceneTransitionAnimation(activity, transitionImage, EXTRA_OBJCT);
+        ActivityCompat.startActivity(activity, intent, options.toBundle());
+    }
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sample_soil_details);
     }
 }
