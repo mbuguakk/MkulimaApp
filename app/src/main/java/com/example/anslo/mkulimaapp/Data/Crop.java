32ea11d2fb5e8b3c182c4cd18524210e40ca635f
Paul Mburu <paulmburu53@gmail.com>
2018-11-06 11:55:09 +0300

    Added the Crop details

--- app/src/main/java/com/example/anslo/mkulimaapp/Data/Crop.java
+++ app/src/main/java/com/example/anslo/mkulimaapp/Data/Crop.java
@@ -5,5 +5,39 @@ package com.example.anslo.mkulimaapp.Data;
  */
 
 public class Crop {
+    private static final int NO_IMAGE_PROVIDED=-1;
+    private String cropName;
+    private String soilPh;
+    private int imageResourceId;
 
+
+    /**
+     * create a new Crop object
+     * @param cropName  name of the crop
+     * @param soilPh  pH value of the crop
+     * @param imageResourceId image of the crop
+     */
+    public Crop(String cropName,String soilPh,int imageResourceId){
+        this.cropName=cropName;
+        this.soilPh=soilPh;
+        this.imageResourceId=imageResourceId;
+    }
+
+    public String getCropName() {
+        return cropName;
+    }
+
+    public String getSoilPh() {
+        return soilPh;
+    }
+
+    public int getImageResourceId() {
+        return imageResourceId;
+    }
+    /**
+     *  Returns whether or not there is an image for this word
+     */
+    public boolean hasImage(){
+        return imageResourceId!=NO_IMAGE_PROVIDED;
+    }
 }
